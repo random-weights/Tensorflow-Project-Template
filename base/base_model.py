c7d0a751573d112bfba9b16c775a9f870b789544
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-07-02 08:41:21 -0400

    removed unnecessary methods basemodel class

--- base/base_model.py
+++ base/base_model.py
@@ -4,10 +4,7 @@ import tensorflow as tf
 class BaseModel:
     def __init__(self, config):
         self.config = config
-        # init the global step
-        self.init_global_step()
-        # init the epoch counter
-        self.init_cur_epoch()
+        self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
 
     # save function that saves the checkpoint in the path defined in the config file
     def save(self, sess):
@@ -23,22 +20,8 @@ class BaseModel:
             self.saver.restore(sess, latest_checkpoint)
             print("Model loaded")
 
-    # just initialize a tensorflow variable to use it as epoch counter
-    def init_cur_epoch(self):
-        with tf.variable_scope('cur_epoch'):
-            self.cur_epoch_tensor = tf.Variable(0, trainable=False, name='cur_epoch')
-            self.increment_cur_epoch_tensor = tf.assign(self.cur_epoch_tensor, self.cur_epoch_tensor + 1)
-
-    # just initialize a tensorflow variable to use it as global step counter
-    def init_global_step(self):
-        # DON'T forget to add the global step tensor to the tensorflow trainer
-        with tf.variable_scope('global_step'):
-            self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
-
     def init_saver(self):
-        # just copy the following line in your child class
-        # self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
-        raise NotImplementedError
+        self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
 
     def build_model(self):
         raise NotImplementedError
