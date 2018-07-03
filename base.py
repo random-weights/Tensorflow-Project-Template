5926000bab1e96bf8c9680485d5083c5be8781bd
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-07-03 09:45:16 -0400

    moved all base classes to single file.

--- base.py
+++ base.py
@@ -1,11 +1,28 @@
-"""
-some attributes of trainer class are hyper params for model
-1. epochs
-2. iter per epochs
-3. learning rate
-4. batch_size
-"""
 import tensorflow as tf
+
+
+class BaseModel:
+	def __init__(self, config):
+		self.config = config
+		self.global_step_tensor = tf.Variable(0, trainable=False, name='global_step')
+		self.saver = tf.train.Saver(max_to_keep=self.config.max_to_keep)
+
+	# save function that saves the checkpoint in the path defined in the config file
+	def save(self, sess):
+		print("Saving model...")
+		self.saver.save(sess, self.config.checkpoint_dir, self.global_step_tensor)
+		print("Model saved")
+
+	# load latest checkpoint from the experiment path defined in the config file
+	def load(self, sess):
+		latest_checkpoint = tf.train.latest_checkpoint(self.config.checkpoint_dir)
+		if latest_checkpoint:
+			print("Loading model checkpoint {} ...\n".format(latest_checkpoint))
+			self.saver.restore(sess, latest_checkpoint)
+			print("Model loaded")
+
+	def build_model(self):
+		raise NotImplementedError
 
 
 class BaseTrain:
