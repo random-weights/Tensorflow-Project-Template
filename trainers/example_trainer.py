f89d0e1824a8b808bec6855c42af2df54620846d
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-07-03 09:39:17 -0400

    remoed config, logger dependence

--- trainers/example_trainer.py
+++ trainers/example_trainer.py
@@ -4,31 +4,31 @@ import numpy as np
 
 
 class ExampleTrainer(BaseTrain):
-    def __init__(self, sess, model, data, config,logger):
-        super(ExampleTrainer, self).__init__(sess, model, data, config,logger)
+	def __init__(self,name,sess,model,data,epochs,iter_per_epoch,batch_size):
+		super(ExampleTrainer, self).__init__(sess,model,data,epochs,iter_per_epoch,batch_size)
+		self.save_train_details(name)
 
-    def train_epoch(self):
-        loop = tqdm(range(self.config.num_iter_per_epoch))
-        losses = []
-        accs = []
-        for _ in loop:
-            loss, acc = self.train_step()
-            losses.append(loss)
-            accs.append(acc)
-        loss = np.mean(losses)
-        acc = np.mean(accs)
+	def train(self):
+		loop = tqdm(range(self.epochs))
+		for _ in loop:
+			self.train_epoch()
 
-        cur_it = self.model.global_step_tensor.eval(self.sess)
-        summaries_dict = {
-            'loss': loss,
-            'acc': acc,
-        }
-        self.logger.summarize(cur_it, summaries_dict=summaries_dict)
-        self.model.save(self.sess)
+	def train_epoch(self):
+		loop = tqdm(range(self.iter_per_epoch))
+		losses = []
+		accs = []
+		for _ in loop:
+			loss, acc = self.train_step()
+			losses.append(loss)
+			accs.append(acc)
+		loss = np.mean(losses)
+		acc = np.mean(accs)
 
-    def train_step(self):
-        batch_x, batch_y = next(self.data.next_batch(self.config.batch_size))
-        feed_dict = {self.model.x: batch_x, self.model.y: batch_y, self.model.is_training: True}
-        _, loss, acc = self.sess.run([self.model.train_step, self.model.cross_entropy, self.model.accuracy],
-                                     feed_dict=feed_dict)
-        return loss, acc
+		self.model.save(self.sess)
+
+	def train_step(self):
+		batch_x, batch_y = next(self.data.next_batch(self.batch_size))
+		feed_dict = {self.model.x: batch_x, self.model.y: batch_y, self.model.is_training: True}
+		_, loss, acc = self.sess.run([self.model.train_step, self.model.cross_entropy, self.model.accuracy],
+									 feed_dict=feed_dict)
+		return loss, acc
