ef9b05ffe63d3eef47bcf207b24efe3e8651e545
Lakshmi Sai Ram Thubati <thubatilakshmi@gmail.com>
2018-07-03 09:11:47 -0400

    new abstract method save_train_details
    also added extra class attributes like epochs, iter_per_epoch and batch_size

--- base/base_train.py
+++ base/base_train.py
@@ -3,38 +3,46 @@ some attributes of trainer class are hyper params for model
 1. epochs
 2. iter per epochs
 3. learning rate
+4. batch_size
 """
-
 import tensorflow as tf
 
 
 class BaseTrain:
-    def __init__(self, sess, model, data):
-        self.model = model
-        self.sess = sess
-        self.data = data
-        self.init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
-        self.sess.run(self.init)
+	def __init__(self, sess, model, train_data,epochs,iter_per_epoch,batch_size):
+		self.model = model
+		self.sess = sess
+		self.data = train_data
+		self.epochs = epochs
+		self.iter_per_epoch = iter_per_epoch
+		self.batch_size = batch_size
 
-    def train(self,epochs):
-        """
-        :param epochs: no. of passes through whole training set
-        :return: None
-        """
-        raise NotImplementedError
+		self.init = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
+		self.sess.run(self.init)
 
-    def train_epoch(self,iter_per_epoch):
-        """
-        implement the logic of epoch:
-        -loop over the number of iterations in the config and call the train step
-        -add any summaries you want using the summary
-        """
-        raise NotImplementedError
+	def save_train_details(self,name):
+		raise NotImplementedError
 
-    def train_step(self,batch_size):
-        """
-        implement the logic of the train step
-        - run the tensorflow session
-        - return any metrics you need to summarize
-        """
-        raise NotImplementedError
+	def train(self):
+		"""
+		implement logic of training process,
+		-what to do after/certain no. of epoch(s)
+		-any console output or progress bar
+		"""
+		raise NotImplementedError
+
+	def train_epoch(self):
+		"""
+		implement the logic of epoch:
+		-loop over the number of iterations in the config and call the train step
+		-add any summaries you want using the summary
+		"""
+		raise NotImplementedError
+
+	def train_step(self):
+		"""
+		implement the logic of the train step
+		- run the tensorflow session
+		- return any metrics you need to summarize
+		"""
+		raise NotImplementedError
